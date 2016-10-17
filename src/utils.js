837118b05160877ceccb63f2d48e83f10085eba4
Marcus Whybrow <m@marcus.codes>
2016-10-17 07:59:41 +0100

    fix isSubset (for real)

--- src/utils.js
+++ src/utils.js
@@ -4,11 +4,11 @@ function isObject(val) {
 
 export function isSubset(candidate, authority) {
   if (!isObject(candidate)) return true;
+  if (!isObject(authority)) return false;
   return Object.keys(candidate).every(candidateKey => {
-    if (!isObject(authority)) return false;
-    if (authority[candidateKey] !== 'undefined') return false;
+    if (authority[candidateKey] === undefined) return false;
     if (typeof candidate[candidateKey] === 'object')
-      return isSubset(authority[candidateKey], candidate[candidateKey]);
+      return isSubset(candidate[candidateKey], authority[candidateKey]);
     return true;
   });
 }
